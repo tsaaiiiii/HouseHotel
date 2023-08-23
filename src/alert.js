d91687233072ce1586e9861f0ae73b8da34c157f
tsaaiiiii <a0909513991@gmail.com>
2023-08-23 14:24:35 +0800

    update dist

--- src/alert.js
+++
@@ -1,7 +0,0 @@
-import Swal from 'sweetalert2'
-
-const alertError = (errorMsg = '有某個地方出錯了！') => {
-  Swal.fire({ text: errorMsg, icon: 'error', position: 'top-end', timer: 1500 })
-}
-
-export { alertError }
